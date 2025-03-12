96a1d1d2f7da5c0adb27e8c125e1652a06452fe6
vishalbhakt <vishalkush9119@gmail.com>
2025-03-12 14:07:51 +0530

    Update

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ob=-0l@+8i8s@_j40x%+t-7+ka1c3)6d-b%!zrrd_nn+j9lr5f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost' '127.0.0.1' '.pythonanywhere.com']
 
 
 # Application definition
